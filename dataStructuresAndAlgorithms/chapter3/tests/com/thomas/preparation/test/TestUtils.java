c29265511df715577425ac73186cfeb488d5b97a
Thomas FItzpatrick <thomasfitzpatrick@Thomass-MacBook-Air.local>
2012-12-10 23:09:24 +0100

    organize imports

--- dataStructuresAndAlgorithms/chapter3/tests/com/thomas/preparation/test/TestUtils.java
+++ dataStructuresAndAlgorithms/chapter3/tests/com/thomas/preparation/test/TestUtils.java
@@ -1,7 +1,6 @@
 package com.thomas.preparation.test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 
