ad45f405ae022418f0bf4d63efa0a705a57241ef
Thomas FItzpatrick <thomasfitzpatrick@Thomass-MacBook-Air.local>
2013-02-10 11:19:38 +0100

    1.1.10 done

--- sedgewick/code/Main.java
+++ sedgewick/code/Main.java
@@ -2,11 +2,18 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		System.out.println('b');
-		System.out.println('b' + 'c');
-		System.out.println((char) 'a' + 4);
+		System.out.println(asBinaryNumber(6));
 
-		
+	}
+
+	private static String asBinaryNumber(int i) {
+
+		String binaryNumber = "";
+		while (i != 0) {
+			binaryNumber = i % 2 + binaryNumber;
+			i = i / 2;
+		}
+		return binaryNumber;
 
 	}
 
