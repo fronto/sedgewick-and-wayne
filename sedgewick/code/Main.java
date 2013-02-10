5d2f54eeb85bc896fe1f7b528f715ce8c5609719
Thomas FItzpatrick <thomasfitzpatrick@Thomass-MacBook-Air.local>
2013-02-10 12:24:26 +0100

    1.1.12 done

--- sedgewick/code/Main.java
+++ sedgewick/code/Main.java
@@ -2,48 +2,20 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		boolean[][] array = randomArray(5);
-		System.out.println(booleanArrayAsString(array));
+		int[] array = new int[10];
 		
+		for(int i = 0; i < 10; i++) {
+			array[i] = 9 - i;
+		}
+		for(int i = 0; i < 10; i++) {
+			array[i] = array[array[i]];
+		}
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i);
+		}
 		
 		
 	}
 
-	public static boolean[][] randomArray(int size) {
 
-		boolean[][] array = new boolean[size][size];
-		for (int i = 0; i < size; i++) {
-			for (int j = 0; j < size; j++) {
-				if (Math.random() < 0.5) {
-					array[i][j] = false;
-					continue;
-				}
-				array[i][j] = true;
-			}
-		}
-		return array;
-
-	}
-
-	public static String booleanArrayAsString(boolean[][] array) {
-		StringBuilder arrayToPrint = new StringBuilder();
-		arrayToPrint.append(' ');
-		for(int t = 0; t < array.length; t++) {
-			arrayToPrint.append(t + " ");
-		}
-		arrayToPrint.append('\n');
-		
-		for (int i = 0; i < array.length; i++) {
-			arrayToPrint.append(i);
-			for (int j = 0; j < array[0].length; j++) {
-				if (array[i][j]) {
-					arrayToPrint.append("* ");
-					continue;
-				}
-				arrayToPrint.append("  ");
-			}
-			arrayToPrint.append('\n');
-		}
-		return arrayToPrint.toString();
-	}
 }
