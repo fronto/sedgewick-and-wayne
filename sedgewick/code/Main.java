628cfc8dff2526ba5a5287f3c07e15a5e10dd7e4
Thomas FItzpatrick <thomasfitzpatrick@Thomass-MacBook-Air.local>
2013-02-10 21:22:25 +0100

    1.1.13 done

--- sedgewick/code/Main.java
+++ sedgewick/code/Main.java
@@ -2,20 +2,44 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		int[] array = new int[10];
-		
-		for(int i = 0; i < 10; i++) {
-			array[i] = 9 - i;
-		}
-		for(int i = 0; i < 10; i++) {
-			array[i] = array[array[i]];
-		}
-		for(int i = 0; i < 10; i++) {
-			System.out.println(i);
-		}
-		
+		int[][] array = contiousArray(2, 3);
+		printIntegerArray(array);
+		array = transpose(array);
+		printIntegerArray(array);
 		
 	}
+	
+	
+	private static int[][] contiousArray(int width, int height) {
+		int[][] array = new int[height][width];
+		int counter = 0;
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) {
+				counter++;
+				array[i][j] = counter;
+			}
+		}
+		return array;
+	}
+	
+	private static void printIntegerArray(int[][] array) {
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[0].length; j++) {
+				System.out.printf("%2d",array[i][j]);
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
 
+	public static int[][] transpose(int[][] array) {
+		int[][] result = new int[array[0].length][array.length];
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[0].length; j++) {
+				result[j][i] = array[i][j];
+			}
+		}
+		return result;
+	}
 
 }
