6dc5da53b3a3b90592de68011d0ad51607b77dba
Thomas FItzpatrick <thomasfitzpatrick@thomasf.lan>
2013-03-10 16:28:00 +0100

    1.1.17 done

--- sedgewick/code/Main.java
+++ sedgewick/code/Main.java
@@ -8,9 +8,10 @@ public class Main {
 	}
 
 	static String exR1(int n) {
+		String s = exR1(n-3) + n + exR1(n-2) + n;
 		if(n <= 0) { 
 			return "";
 		}
-		return exR1(n-3) + n + exR1(n-2) + n;
+		return s;
 	}
 }
