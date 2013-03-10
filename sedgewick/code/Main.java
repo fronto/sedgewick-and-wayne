65e3d6ba67b6a3880128669266c050bae127a5e1
Thomas FItzpatrick <thomasfitzpatrick@thomasf.local>
2013-03-10 19:38:50 +0100

    1.1.18 as addition

--- sedgewick/code/Main.java
+++ sedgewick/code/Main.java
@@ -2,16 +2,33 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		System.out.println(exR1(6));	
-
+		mystery(2, 25);				
+		System.out.println();
+		mystery(3, 11);
 
 	}
 
-	static String exR1(int n) {
-		String s = exR1(n-3) + n + exR1(n-2) + n;
-		if(n <= 0) { 
-			return "";
+	public static int mystery(int a, int b) {
+		
+		if(b == 0) {
+			int result = 0;
+			System.out.println("mystery(" + a + "," + b + ") -> " + result);
+			return result;
 		}
-		return s;
+
+		if(b % 2 == 0) {
+			int result = mystery(a+a, b/2);	
+			System.out.println("mystery(" + a + "," + b + ") -> " + result);
+			return result;
+
+		}
+
+		
+		int result = mystery(a+a, b/2) + a;	
+		System.out.println("mystery(" + a + "," + b + ") -> " + result);
+		return result;
+
 	}
+
+
 }
