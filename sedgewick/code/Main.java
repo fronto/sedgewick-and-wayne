dad1bef536176d5c93ec5425c7040981ae897038
Thomas FItzpatrick <thomasfitzpatrick@thomasf.local>
2013-03-10 20:06:47 +0100

    1.1.19 the slow version

--- sedgewick/code/Main.java
+++ sedgewick/code/Main.java
@@ -2,33 +2,6 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		mystery(2, 5);				
-		System.out.println();
-		mystery(3, 11);
-
 	}
-
-	public static int mystery(int a, int b) {
-		
-		if(b == 0) {
-			int result = 1;
-			System.out.println("mystery(" + a + "," + b + ") -> " + result);
-			return result;
-		}
-
-		if(b % 2 == 0) {
-			int result = mystery(a*a, b/2);	
-			System.out.println("mystery(" + a + "," + b + ") -> " + result);
-			return result;
-
-		}
-
-		
-		int result = mystery(a*a, b/2) * a;	
-		System.out.println("mystery(" + a + "," + b + ") -> " + result);
-		return result;
-
-	}
-
 
 }
--- sedgewick/code/run.sh
+++ sedgewick/code/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 javac -cp lib/stdlib.jar:. *.java
-java -cp lib/stdlib.jar:. Main
+java -cp lib/stdlib.jar:. ${1}
