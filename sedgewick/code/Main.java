6df538f6a687818c116d03e0327e0c677e1b5aaf
Thomas FItzpatrick <thomasfitzpatrick@thomasf.lan>
2013-03-17 15:26:14 +0100

    1.1.20 done

--- sedgewick/code/Main.java
+++ sedgewick/code/Main.java
@@ -1,6 +1,35 @@
 public class Main {
 
+	static double logFactorial(int n) {
+
+		if(n == 1) {
+			return 0.0;
+		}
+
+		return Math.log((double) n) + logFactorial(n -1);
+
+	}
+
+	static int factorial(int n) {
+
+		if(n == 0) {
+			return 1;
+		}
+
+		if(n == 1) {
+			return 1;
+		}
+
+		return n * factorial(n-1);
+
+	}
+
 	public static void main(String[] args) {
+
+		double expected = Math.log((double) factorial(3));	
+		double actual = logFactorial(3);
+
+		System.out.printf("expected: %f  actual: %f\n", expected, actual);
 
 	}
 
