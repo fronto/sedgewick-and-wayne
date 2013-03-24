e0564a74be5fcba1aff9852a55093d3f3af33215
Thomas FItzpatrick <thomasfitzpatrick@thomasf.local>
2013-03-25 00:08:14 +0100

    fixed bugs 1.1.22 done

--- sedgewick/code/src/BinarySearch.java
+++ sedgewick/code/src/BinarySearch.java
@@ -1,15 +1,21 @@
 public class BinarySearch {
 
+	static int numberOfcalls;
+	
+	
+	
 	public static int rank(int key, int[] a) {
 
 		int start = 0;
 		int end = a.length - 1;
-
+		numberOfcalls = 0;
 		return rankRecursive(key, a, start, end);
 	}
 
-	private static int rankRecursive(int key, int[] a, int hi, int lo) {
+	private static int rankRecursive(int key, int[] a, int lo, int hi) {
 
+		printRecursive(lo, hi);
+		
 		int index = (hi + lo) / 2;
 
 		if (a[index] == key) {
@@ -27,4 +33,17 @@ public class BinarySearch {
 		return -1;
 	}
 
+	private static void printRecursive(int lo, int hi) {
+		
+		numberOfcalls++;
+		StringBuilder stringBuilder = new StringBuilder();
+		for(int i = 0; i < numberOfcalls; i ++) {
+			stringBuilder.append(" ");
+		}
+		stringBuilder.append("rank(").append(lo).append(", ").append(hi).append(")");
+		
+		System.out.println(stringBuilder);
+		
+	}
+	
 }
