2360c01fc0c8414e86d5c99a5316ebd4c6f8120c
Thomas FItzpatrick <thomasfitzpatrick@thomasf.local>
2013-03-24 20:38:41 +0100

    now using In and Out library classes 1.1.21 done

--- sedgewick/code/src/Main.java
+++ sedgewick/code/src/Main.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.util.Scanner;
 
 public class Main {
 
@@ -9,12 +10,28 @@ public class Main {
 			throw new IllegalArgumentException("non-existant file: "
 					+ file.getAbsolutePath());
 		}
-		
-		Parser parser = Parser.createParser(file);
-		while (parser.canParseNextLine()) {
-			InputLine inputLine = parser.parseNextLine();
-			OutputLine outputLine = OutputLine.compute(inputLine);
-			outputLine.write(System.out);
+
+		// Parser parser = Parser.createParser(file);
+		// while (parser.canParseNextLine()) {
+		// InputLine inputLine = parser.parseNextLine();
+		// OutputLine outputLine = OutputLine.compute(inputLine);
+		// outputLine.write(System.out);
+		// }
+
+		In fileIn = new In(file);
+		Out out = new Out();
+		while (fileIn.hasNextLine()) {
+
+			String line = fileIn.readLine();
+			// In lineIn = new In(line);
+			Scanner scanner = new Scanner(line);
+			if (scanner.hasNext()) {
+				String name = scanner.next();
+				Integer first = scanner.nextInt();
+				Integer second = scanner.nextInt();
+				double result = (double) first / second;
+				out.printf("%s\t%d %d %.3f\n", name, first, second, result);
+			}
 		}
 
 	}
