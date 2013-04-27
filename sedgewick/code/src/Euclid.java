1800043024b9673d4a9ccc8a06966fc98abe3ef4
Thomas FItzpatrick <thomasfitzpatrick@thomasf.lan>
2013-04-27 13:44:53 +0200

    1.1.24 done

--- sedgewick/code/src/Euclid.java
+++ sedgewick/code/src/Euclid.java
@@ -3,30 +3,72 @@ public class Euclid {
 
 	public static void main(String[] args) {
 
-		int dividend = 105;	
-		int divisor = 24;
-		
-		
-		int hcd = findHCD(dividend, divisor);
-		System.out.println("HCD is " + hcd);	
+		//assertEqual(3, findHCD(105, 24));
+		//assertEqugl(3, findHCD(24, 105));
+		//assertEqual(105, findHCD(105, 105));
+		//assertEqual(1, findHCD(31, 32));
+		//System.out.println("HCD is " + hcd);	
+
+		int dividend = Integer.parseInt(args[0]);
+		int divisor = Integer.parseInt(args[1]);
+
+		findHCD(dividend, divisor);
+
+
 	}
 
 
+	private static void assertEqual(Integer expected, Integer actual) {
+		if(expected.equals(actual)) {
+			return;
+		}
+		throw new RuntimeException("expected value: " + expected + " but got " + actual);
+	}
+
 	public static int findHCD(int dividend, int divisor) {
+
+		int result = findHCDRecursive(dividend, divisor);
+		System.out.println("findHCD(" + dividend + ", " + divisor + ") is " + result);
+		return result;
+
+	}
+
+
+	public static int findHCDUsingWhileLoop(int dividend, int divisor) {
+		String message = "findHCD(" + dividend + ", " + divisor +  ")";
 		int remainder = findRemainder(dividend, divisor);
-		while(remainder != 0) {
+		while(true) {
+			if(remainder == 0) {
+				break;
+			}
 			dividend = divisor;
 			divisor = remainder;
 			remainder = findRemainder(dividend, divisor);
 		}
+		message = message + " is " + divisor;
+		System.out.println(message);
 		return divisor;
+	}
+
+	public static int findHCDRecursive(int dividend, int divisor) {
+
+		int remainder = dividend % divisor;
+		
+		if(remainder == 0) {
+			return divisor;
+		}
+
+		return findHCDRecursive(divisor, remainder);
+		
 	}
 
 
 	private static int findRemainder(int dividend, int divisor) {
 		int result = dividend % divisor;
-		System.out.println(dividend + " % " + divisor + " = " + result);
+		//System.out.println(dividend + " % " + divisor + " = " + result);
 		return result;
 	}
 
+	
+	
 }
