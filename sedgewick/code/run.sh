e24f6cdc70d4174075bcf60dac7a382d243c387f
Thomas FItzpatrick <thomasfitzpatrick@thomasf.lan>
2013-05-02 13:25:55 +0200

    new folder booksite to hold values from the booksite

--- sedgewick/code/run.sh
+++ sedgewick/code/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 rm src/*.class
-CLASSPATH="lib/stdlib.jar:./src"
-javac -cp $CLASSPATH  ./src/*.java
+CLASSPATH="lib/stdlib.jar:./src:./booksite"
+javac -cp $CLASSPATH  ./src/*.java ./booksite/*java
 java -cp $CLASSPATH ${*}
