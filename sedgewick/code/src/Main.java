50907b67bfae815e9a5a1e010368ffa5343adfcf
Thomas FItzpatrick <thomasfitzpatrick@thomasf.lan>
2013-05-03 13:45:32 +0200

    1.2.5 done

--- sedgewick/code/src/Main.java
+++ sedgewick/code/src/Main.java
@@ -5,14 +5,10 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		//world
-		//hello
-		String string1 = "hello";
-		String string2 = string1;
-		string1 = "world";
-		StdOut.println(string1);
-		StdOut.println(string2);
-		
+		String s = "Hello World";	
+		s.toUpperCase();
+		s.substring(6, 11);
+		StdOut.println(s);
 
 	}
 
