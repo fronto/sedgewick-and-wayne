8bb1789d6d98e28ce5fcd736b3ea3c593f0f5185
Thomas FItzpatrick <thomasfitzpatrick@thomasf.lan>
2013-05-03 14:57:24 +0200

    1.2.9 done (I think)

--- sedgewick/code/booksite/BinarySearch.java
+++ sedgewick/code/booksite/BinarySearch.java
@@ -27,11 +27,12 @@ import java.util.Arrays;
 public class BinarySearch {
 
     // precondition: array a[] is sorted
-    public static int rank(int key, int[] a) {
+    public static int rank(int key, int[] a, Counter counter) {
         int lo = 0;
         int hi = a.length - 1;
         while (lo <= hi) {
             // Key is in a[lo..hi] or not present.
+        	counter.increment();
             int mid = lo + (hi - lo) / 2;
             if      (key < a[mid]) hi = mid - 1;
             else if (key > a[mid]) lo = mid + 1;
@@ -44,12 +45,15 @@ public class BinarySearch {
         int[] whitelist = In.readInts(args[0]);
 
         Arrays.sort(whitelist);
-
         // read key; print if not in whitelist
+        Counter counter = new Counter(" keys searched in total");
         while (!StdIn.isEmpty()) {
             int key = StdIn.readInt();
-            if (rank(key, whitelist) == -1)
+            if (rank(key, whitelist, counter) == -1) {
                 StdOut.println(key);
+            }
+            
         }
+        System.out.println(counter.toString());
     }
 }
