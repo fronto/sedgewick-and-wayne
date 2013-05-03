9a005503e0bfd901ac6a72badf8e29d87f50bc76
Thomas FItzpatrick <thomasfitzpatrick@thomasf.lan>
2013-05-03 13:59:50 +0200

    1.2.6 done

--- sedgewick/code/src/Main.java
+++ sedgewick/code/src/Main.java
@@ -1,15 +1,24 @@
-import java.io.File;
-import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		String s = "Hello World";	
-		s.toUpperCase();
-		s.substring(6, 11);
-		StdOut.println(s);
+		String s  = args[0];
+		String t = args[1];
+		
+		if(isCircularRotation(s,t)) {
+			System.out.println(s + " is a circular rotation of " + t);
+			return;
+		} 
+		
+		System.out.println(s + " is NOT a circular rotation of " + t);
+	}
 
+	private static boolean isCircularRotation(String s, String t) {
+
+		return s.length() == t.length() ? (s + s).indexOf(t) != -1 : false;
+		
+		
 	}
 
 }
