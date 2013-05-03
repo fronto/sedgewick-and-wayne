4002b00504e4aa6098033b8982b88e2e9191c762
Thomas FItzpatrick <thomasfitzpatrick@thomasf.lan>
2013-05-03 13:35:40 +0200

    1.2.4 done

--- sedgewick/code/src/Main.java
+++ sedgewick/code/src/Main.java
@@ -5,34 +5,14 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		File file = new File(args[0]);
-		if (!file.exists()) {
-			throw new IllegalArgumentException("non-existant file: "
-					+ file.getAbsolutePath());
-		}
-
-		// Parser parser = Parser.createParser(file);
-		// while (parser.canParseNextLine()) {
-		// InputLine inputLine = parser.parseNextLine();
-		// OutputLine outputLine = OutputLine.compute(inputLine);
-		// outputLine.write(System.out);
-		// }
-
-		In fileIn = new In(file);
-		Out out = new Out();
-		while (fileIn.hasNextLine()) {
-
-			String line = fileIn.readLine();
-			// In lineIn = new In(line);
-			Scanner scanner = new Scanner(line);
-			if (scanner.hasNext()) {
-				String name = scanner.next();
-				Integer first = scanner.nextInt();
-				Integer second = scanner.nextInt();
-				double result = (double) first / second;
-				out.printf("%s\t%d %d %.3f\n", name, first, second, result);
-			}
-		}
+		//world
+		//hello
+		String string1 = "hello";
+		String string2 = string1;
+		string1 = "world";
+		StdOut.println(string1);
+		StdOut.println(string2);
+		
 
 	}
 
