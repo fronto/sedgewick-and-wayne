dc8289c891b5589955416931470732074478c50f
Thomas FItzpatrick <thomasfitzpatrick@thomasf.lan>
2013-05-04 15:34:17 +0200

    1.2.14 done

--- sedgewick/code/src/Transaction.java
+++ sedgewick/code/src/Transaction.java
@@ -3,26 +3,46 @@ public class Transaction {
 	private final String who;
 	private final Date when;
 	private final double amount;
-	
+
 	public Transaction(String who, Date when, double amount) {
 		this.who = who;
 		this.when = when;
 		this.amount = amount;
 	}
-	
-	
+
 	public String who() {
 		return who;
 	}
-	
+
 	public Date when() {
 		return when;
 	}
-	
-	
+
 	public double amount() {
 		return amount;
 	}
-	
-	
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (this.getClass() != obj.getClass()) {
+			return false;
+		}
+		Transaction that = (Transaction) obj;
+		if (!this.who.equals(that.who)) {
+			return false;
+		}
+		if (!this.when.equals(that.when)) {
+			return false;
+		}
+		if (!(this.amount != that.amount)) {
+			return false;
+		}
+		return true;
+	}
 }
