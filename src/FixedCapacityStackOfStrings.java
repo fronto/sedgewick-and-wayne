e5eff453df544e29dbebf79dbd7ccffb7fcd5e96
Thomas FItzpatrick <thomasfitzpatrick@thomasf.local>
2014-04-20 13:14:01 +0200

    implement Fixed capactity stack of strings

--- src/FixedCapacityStackOfStrings.java
+++ src/FixedCapacityStackOfStrings.java
@@ -1,13 +1,39 @@
-public class FixedCapacityStackOfStrings<T> {
+public class FixedCapacityStackOfStrings implements Stack<String> {
 
-	private final T[] items;
+    private int index;
+	private final String[] items;
+
+
 
 	@SuppressWarnings("unchecked")
 	public FixedCapacityStackOfStrings(int capacity) {
-		this.items =  (T[]) new Object[capacity];
+		this.items =  new String[capacity];
+        this.index = 0;
 	}
 
 
+    @Override
+    public void push(String s) {
+        items[index++] = s;
+    }
 
+    @Override
+    public String pop() {
+        return items[index--];
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return index == 0;
+    }
+
+    @Override
+    public int size() {
+        return index;
+    }
+
+    boolean isFull() {
+        return index == items.length;
+    }
 
 }
