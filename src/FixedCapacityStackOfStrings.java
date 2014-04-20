a740d755d2e50872e2f6fcad0b0079c0bd64f1c2
Thomas FItzpatrick <thomasfitzpatrick@thomasf.lan>
2014-04-20 15:33:49 +0200

    1.3.1 done

--- src/FixedCapacityStackOfStrings.java
+++ src/FixedCapacityStackOfStrings.java
@@ -1,39 +1,61 @@
-public class FixedCapacityStackOfStrings implements Stack<String> {
+public class FixedCapacityStackOfStrings {
 
     private int index;
-	private final String[] items;
+    private final String[] items;
 
 
-
-	@SuppressWarnings("unchecked")
-	public FixedCapacityStackOfStrings(int capacity) {
-		this.items =  new String[capacity];
+    @SuppressWarnings("unchecked")
+    public FixedCapacityStackOfStrings(int capacity) {
+        this.items = new String[capacity];
         this.index = 0;
-	}
+    }
 
 
-    @Override
     public void push(String s) {
         items[index++] = s;
     }
 
-    @Override
     public String pop() {
         return items[index--];
     }
 
-    @Override
     public boolean isEmpty() {
         return index == 0;
     }
 
-    @Override
     public int size() {
         return index;
     }
 
     boolean isFull() {
         return index == items.length;
+    }
+
+
+    public static void main(String[] args) {
+
+
+        FixedCapacityStackOfStrings fixedCapacityStackOfStrings = new FixedCapacityStackOfStrings(2);
+        fixedCapacityStackOfStrings.push("1");
+        assertFalse(fixedCapacityStackOfStrings.isFull());
+
+        fixedCapacityStackOfStrings.push("2");
+        assertTrue(fixedCapacityStackOfStrings.isFull());
+
+    }
+
+    private static void assertFalse(boolean truth) {
+        if(truth) {
+            throw new RuntimeException("expected false");
+        }
+    }
+
+    private static void assertTrue(boolean truth) {
+        if (truth) {
+            return;
+        }
+
+        throw new RuntimeException("failed assertion");
     }
 
 }
